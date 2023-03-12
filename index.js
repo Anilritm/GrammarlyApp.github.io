dc1e750d39bb1dacf29cbce4653a75e8055879eb
Anilritm <anils984933@gmail.com>
2023-03-12 10:29:55 +0530

    My Grammarly Website

---
+++ index.js
@@ -0,0 +1,25 @@
+const charVal = document.getElementById("textarea");
+let totalCount  = document.getElementById("total-counter");
+let remainingCount = document.getElementById("remain-counter");
+
+let char =0;
+
+
+const updateCounter = () =>{
+  userChar = charVal.value.length;
+
+  totalCount.innerText = userChar;
+
+  remainingCount.innerText = 150-userChar;
+
+};
+ 
+charVal.addEventListener("keyup" , ()=> updateCounter() );
+
+const copyText =()=>
+{
+   charVal.select();
+   charVal.setSelectionRange(0, 9999);
+   navigator.clipboard.writeText(charVal.value);
+
+};
